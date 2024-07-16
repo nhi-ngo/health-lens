fcfc1ff0cc955e5970f2ebaf101c1db33259a609
nhi-ngo <nhingo.y@gmail.com>
2024-07-16 13:35:24 -0400

    Initial Commit

---
+++ HealthLens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthLens
+//
+//  Created by Nhi Ngo on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthLens/HealthLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthLensApp.swift
+//  HealthLens
+//
+//  Created by Nhi Ngo on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
