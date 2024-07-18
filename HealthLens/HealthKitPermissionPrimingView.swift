1901a309a3471326321a42bc2db5e29eacaa11c4
nhi-ngo <nhingo.y@gmail.com>
2024-07-18 11:01:02 -0400

    HealthKit permission priming UI

---
+++ HealthLens/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,46 @@
+//
+//  HealthKitPermissionPrimingView.swift
+//  HealthLens
+//
+//  Created by Nhi Ngo on 7/18/24.
+//
+
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    
+    var description = """
+    This app displays your step and weight data in interactive charts.
+    
+    You can also add new step or weight data to Apple Health from this app. Your data is private and secured.
+    """
+    
+    var body: some View {
+        VStack(spacing: 130) {
+            VStack(alignment: .leading, spacing: 10) {
+                Image(.appleHealth)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3), radius: 16)
+                    .padding(.bottom, 12)
+                
+                Text("Apple Health Integration")
+                    .font(.title2).bold()
+                
+                Text(description)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Button("Connect Apple Health") {
+                // Code
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
