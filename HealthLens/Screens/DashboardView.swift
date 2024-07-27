b96748eeadabf60eaa89fab5ceeee80797952a00
nhi-ngo <nhingo.y@gmail.com>
2024-07-27 12:40:59 -0400

    reorganize project - step bar chart

---
+++ HealthLens/Screens/DashboardView.swift
@@ -0,0 +1,90 @@
+//
+//  DashboardView.swift
+//  HealthLens
+//
+//  Created by Nhi Ngo on 7/16/24.
+//
+
+import SwiftUI
+import Charts
+
+enum HealthMetricContext: String, CaseIterable, Identifiable {
+    case steps, weight
+    var id: Self { self }
+    
+    var tint: Color {
+        switch self {
+        case .steps:
+            Color.pink
+        case .weight:
+            Color.indigo
+        }
+    }
+}
+
+
+struct DashboardView: View {
+    
+    @Environment(HealthKitManager.self) private var hkManager
+    @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
+    @State private var isShowingPermissionPrimingSheet = false
+    @State private var selectedStat: HealthMetricContext = .steps
+    @State private var rawSelectedDate: Date?
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) {
+                            Text($0.rawValue.capitalized)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    StepBarChart(selectedStat: selectedStat, chartData: hkManager.stepData)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            Label("Averages", systemImage: "calendar")
+                                .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                            
+                            Text("Last 28 days")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+            }
+            .padding()
+            .task {
+                await hkManager.fetchStepCount()
+                isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
+            }
+            .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                HealthDataListView(metric: metric)
+            }
+            .fullScreenCover(isPresented: $isShowingPermissionPrimingSheet, onDismiss: {
+                // fetch health data
+            }, content: {
+                HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
+            })
+        }
+        .tint(selectedStat.tint)
+    }
+}
+
+#Preview {
+    DashboardView()
+        .environment(HealthKitManager())
+}
